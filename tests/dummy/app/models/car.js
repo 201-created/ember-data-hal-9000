bbd134fd8ae388eaef2cab22eb587f14b8f7787a
Cory Forsyth <cory.forsyth@gmail.com>
2014-11-19 12:07:57 -0700

    Add support for async hasMany relationships via _links

---
+++ tests/dummy/app/models/car.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  make:   DS.attr('string'),
+  model:  DS.attr('string'),
+  wheels: DS.hasMany('wheel', {async:true})
+});
---
+++ tests/dummy/app/models/wheel.js
@@ -0,0 +1,6 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  hasSnowChains: DS.attr('boolean'),
+  car: DS.belongsTo('car', {async:true})
+});
