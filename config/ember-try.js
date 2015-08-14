6df46cb0621122a1f48e29fd1d5667110bc6982c
Cory Forsyth <cory.forsyth@gmail.com>
2015-08-14 16:33:53 -0400

    Add 1-13-9, release, beta, canary embery-try scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -9,10 +9,45 @@ module.exports = {
     {
       name: "ember-data-1-13-4",
       dependencies: {
-        "ember-data": "components/ember-data#1.13.4"
-      },
-      resolution: {
+        "ember": "1.13.8",
         "ember-data": "1.13.4"
+      }
+    },
+    {
+      name: "ember-data-1-13-9",
+      dependencies: {
+        "ember": "1.13.8",
+        "ember-data": "1.13.9"
+      }
+    },
+    {
+      name: "release",
+      dependencies: {
+        "ember": "components/ember#release",
+        "ember-data": "2.0.0-beta.1"
+      },
+      resolutions: {
+        "ember": "release"
+      }
+    },
+    {
+      name: "beta",
+      dependencies: {
+        "ember": "components/ember#beta",
+        "ember-data": "2.0.0-beta.2"
+      },
+      resolutions: {
+        "ember": "beta"
+      }
+    },
+    {
+      name: "canary",
+      dependencies: {
+        "ember": "components/ember#canary",
+        "ember-data": "2.0.0-beta.2"
+      },
+      resolutions: {
+        "ember": "canary"
       }
     }
   ]
